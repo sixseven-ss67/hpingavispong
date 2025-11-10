7052c73f87a47e63161b532bf2ec6f6c5cf7d07c
sixseven-ss67 <viliopurwana@gmail.com>
2025-11-10 20:11:18 +0700

    Add files via upload

---
+++ pingponggock.py
@@ -0,0 +1,105 @@
+from pygame import *
+class GameSprite(sprite.Sprite):
+    #class constructor
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #Call for the class (Sprite) constructor:
+        sprite.Sprite.__init__(self)
+
+
+        #every sprite must store the image property
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+
+        #every sprite must have the rect property that represents the rectangle it is fitted in
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    #method drawing the character on the window
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    #method to control the sprite with arrow keys
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < h - 80:  
+            self.rect.y += self.speed
+    #method to "shoot" (use the player position to create a bullet there)
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < h - 80:  
+            self.rect.y += self.speed
+    #method to "shoot" (use the player position to create a bullet there)
+back = (200, 255, 255) #background color (background)
+w = 600
+h = 500
+window = display.set_mode((w, h))
+window.fill(back)
+
+#flags responsible for game state
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+#creating ball and paddles   
+racket1 = Player('racket.png', 30, 200, 50, 150, 4) 
+racket2 = Player('racket.png', 520, 200, 50, 150, 4)
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 4)
+
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        
+        #if the ball reaches screen edges, change its movement direction
+        if ball.rect.y > h-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+
+        #if ball flies behind this paddle, display loss condition for player 1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+
+        #if the ball flies behind this paddle, display loss condition for player 2
+        if ball.rect.x > w:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
